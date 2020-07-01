ccb77f05ddf5c3beb4efd024688d3987345b4f00
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-01 13:40:06 +0530

    spring security and basic auth added

--- src/main/java/com/todo/backendAPI/todoRestApi/todo/ToDoResource.java
+++ src/main/java/com/todo/backendAPI/todoRestApi/todo/ToDoResource.java
@@ -3,7 +3,6 @@ package com.todo.backendAPI.todoRestApi.todo;
 import java.net.URI;
 import java.util.List;
 
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
