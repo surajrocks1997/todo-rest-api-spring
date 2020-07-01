7b03afa111d302e0f47c2f91c93b25d00e21a3de
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-01 12:55:58 +0530

    post put and delete todo added

--- src/main/java/com/todo/backendAPI/todoRestApi/todo/Todo.java
+++ src/main/java/com/todo/backendAPI/todoRestApi/todo/Todo.java
@@ -9,6 +9,8 @@ public class Todo {
 	private String description;
 	private Date targetDate;
 	private boolean isDone;
+	
+	protected Todo() {}
 
 	public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
 		super();
